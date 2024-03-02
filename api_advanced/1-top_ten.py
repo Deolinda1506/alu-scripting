639bfd02440548f65d06b45027ba1bd90f107ebf
Gnon Déolinda Bio Bogore <139719649+Deolinda1506@users.noreply.github.com>
2024-03-02 18:01:24 +0300

    Update 1-top_ten.py

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,29 +1,12 @@
 #!/usr/bin/python3
-'''
-Defines function that prints the top ten posts of a subreddit
-'''
-import requests
+"""
+1-main
+"""
+import sys
 
-
-def top_ten(subreddit):
-    '''Prints the top ten posts of a subreddit
-
-    Return:
-        None -  if the subreddit is invalid
-    '''
-    if subreddit is None or not isinstance(subreddit, str):
-        print(None)
-    endpoint = 'https://www.reddit.com'
-    headers = {'user-agent': '0x16-api_advanced:project:\
-v1.0.0 (by /u/shobi_ola)'}
-    params = {'limit': 10}
-    info = requests.get('{}/r/{}/hot.json'.format(endpoint, subreddit),
-                        allow_redirects=False,
-                        headers=headers,
-                        params=params)
-    if info.status_code == 200:
-        json_info = info.json()
-        for post in json_info.get('data').get('children'):
-            print(post.get('data').get('title'))
+if __name__ == '__main__':
+    top_ten = __import__('1-top_ten').top_ten
+    if len(sys.argv) < 2:
+        print("Please pass an argument for the subreddit to search.")
     else:
-        print(None)
+        top_ten(sys.argv[1])
